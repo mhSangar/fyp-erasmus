c522b8c57812dc42cb18f045ac9c6b7926cf1bd7
Mario Sanchez <17150868@studentmail.ul.ie>
2018-02-08 14:37:26 +0000

    quit config file and cleaning

--- pi-video_streaming/stream/mjpeg-server.py
+++ pi-video_streaming/stream/mjpeg-server.py
@@ -16,25 +16,24 @@ import time
 import os
 import picamera
 
-SERVERS = {}
+imageDir = "/tmp/streaming"
+port = 8001
+
 want_send = Semaphore(0)
 want_capt = Semaphore(1)
 
 class RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         # Get info
-        client = self.client_address
-        port = self.server.server_port
-        imageDir = SERVERS[port]["images"]
-
-        logging.info( "Serving client %s:%s from port %s", client[0], client[1], port )
+        client = self.client_address        
+        logging.info("Serving client %s:%s from port %s", client[0], client[1], port)
 
         # Send headers
-        self.send_response( 200 )
-        self.send_header( "Cache-Control", "no-cache" )
-        self.send_header( "Pragma", "no-cache" )
-        self.send_header( "Connection", "close" )
-        self.send_header( "Content-Type", "multipart/x-mixed-replace; boundary=--myboundary" )
+        self.send_response(200)
+        self.send_header("Cache-Control", "no-cache")
+        self.send_header("Pragma", "no-cache")
+        self.send_header("Connection", "close")
+        self.send_header("Content-Type", "multipart/x-mixed-replace; boundary=--myboundary")
         self.end_headers()
 
         o = self.wfile
@@ -46,43 +45,42 @@ class RequestHandler(BaseHTTPRequestHandler):
 
                 want_send.acquire()
 
-                f = open( os.path.join(imageDir, 'img%02d.jpeg' % index) )
+                f = open(os.path.join(imageDir, 'img%02d.jpeg' % index))
                 contents = f.read()
                 f.close()
 
                 want_capt.release()
 
                 try:
-                    logging.debug( "Serving frame %02d", index )
-                    o.write( "--myboundary\r\n" )
-                    o.write( "Content-Type: image/jpeg\r\n" )
-                    o.write( "Content-Length: %s\r\n" % len(contents) )
-                    o.write( "\r\n" )
-                    o.write( contents )
-                    o.write( "\r\n" )
+                    o.write("--myboundary\r\n")
+                    o.write("Content-Type: image/jpeg\r\n")
+                    o.write("Content-Length: %s\r\n" % len(contents))
+                    o.write("\r\n")
+                    o.write(contents)
+                    o.write("\r\n")
                 except:
-                    logging.info( "Done serving client %s:%s from port %s", client[0], client[1], port )
+                    logging.info("Done serving client %s:%s from port %s", client[0], client[1], port)
                     return
 
                 index += 1
 
-            logging.info( "Buffer completed, re-looping" )
-            logging.info( "FPS: %.02f \n" % ((time.time() - prev_completed_buff) / 10) )
+            logging.info("Buffer completed, re-looping")
+            logging.info("FPS: %.02f \n" % (10 / (time.time() - prev_completed_buff)))
             prev_completed_buff = time.time()
 
-class ThreadingHTTPServer( ThreadingMixIn, HTTPServer ):
+class ThreadingHTTPServer(ThreadingMixIn, HTTPServer):
     pass
 
-def startServer( port ):
-    def target( port ):
-        server = ThreadingHTTPServer( ("0.0.0.0",port), RequestHandler )
+def startServer():
+    def target():
+        server = ThreadingHTTPServer(("0.0.0.0", port), RequestHandler)
         server.serve_forever()
 
-    t = Thread( target=target, args=[port] )
+    t = Thread(target=target)
     t.start()
 
-def capture_images( port ):
-    def target( port ):
+def capture_images():
+    def target():
         with picamera.PiCamera() as camera:
             camera.resolution = (480, 360)
             camera.rotation = 180
@@ -95,30 +93,24 @@ def capture_images( port ):
             while True:
                 for index in range(10):
                     want_capt.acquire()
-                    camera.capture( os.path.join(imageDir, 'img%02d.jpeg' % index) )
+                    camera.capture(os.path.join(imageDir, 'img%02d.jpeg' % index))
                     want_send.release()
 
                     index += 1
 
-    t = Thread( target=target, args=[port] )
+    t = Thread(target=target)
     t.start()
 
 if __name__ == "__main__":
-    logging.basicConfig( level=logging.INFO, format="%(asctime)s %(levelname)s [%(name)s] %(message)s" )
+    # switch on the log notifications
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s [%(name)s] %(message)s")
 
-    config = ConfigParser.ConfigParser()
-    config.read( "/home/pi/Documents/python-scripts/video-streaming/mjpeg-server.conf" )
+    # creates the path if it doesn't exists
+    if not os.path.exists(imageDir):
+        os.makedirs(imageDir)
 
-    for section in config.sections():
-        imagesDirectory = config.get( section, "images" )
-        port   = config.getint( section, "port" )
+    capture_images()
+    logging.info("Capturing images in '%s'" % imageDir)
 
-        if not os.path.exists(imagesDirectory):
-            os.makedirs(imagesDirectory)
-
-        capture_images( port )
-        logging.info( "Capturing images in '%s'" % imagesDirectory )
-
-        SERVERS[port] = { "images": imagesDirectory }
-        startServer( port )
-        logging.info( "Serving '%s' on port %s" % (section, port) )
+    startServer()
+    logging.info("Serving 'pi-camera' on port %s" % port)
